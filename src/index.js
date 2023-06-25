dc20a9d16e232beaf817c8bda5f37275ce70d1fd
turku <turkucakmak@hotmail.com>
2023-06-26 00:12:07 +0300

    Completed Assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,25 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  let form = document.querySelector('form')
+  form.addEventListener('submit', (e) => {
+    e.preventDefault()
+    buildToDo(e.target.newtaskdescription.value)
+    form.reset()
+    
+  })
 });
+
+function buildToDo(todo){
+  let p = document.createElement('p');
+  let btn = document.createElement('button');
+  btn.addEventListener('click', handleDelete);
+  btn.textContent ='x';
+  p.textContent = `${todo}`;
+  p.appendChild(btn);
+  console.log(p);
+  document.querySelector('#tasks').appendChild(p);
+
+}
+
+function handleDelete(e){
+  e.target.parentNode.remove()
+}
